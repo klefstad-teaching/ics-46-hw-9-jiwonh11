e6a177b4ae36c2bfb0b0c907324ac8dd5e36741a
Jiwon Hong <jiwonh11@uci.edu>
2025-03-14 06:00:32 +0000

    fixed errors

--- src/ladder_main.cpp
+++ src/ladder_main.cpp
@@ -5,13 +5,12 @@ int main() {
     load_words(word_list, "words.txt");
 
     string start, end;
-    cout << "Enter first word: ";
+    cout << "Enter start word: ";
     cin >> start;
     cout << "Enter end word: ";
     cin >> end;
-
     vector<string> ladder = generate_word_ladder(start, end, word_list);
-    cout << "Word ladder from "<< start << " to " << end << ":" << endl;
+    cout << "Word ladder from " << start << " to " << end << ":" << endl;
     print_word_ladder(ladder);
 
     return 0;
