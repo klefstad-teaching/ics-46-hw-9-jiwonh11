347b9f9149e70da18bf02cfe92fa2749334ba572
Jiwon Hong <jiwonh11@uci.edu>
2025-03-14 05:05:59 +0000

    fixed erros

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -62,5 +62,6 @@ void print_path(const vector<int>& v, int total) {
             cout << " ";
         }
     }
-    cout << "\nTotal cost is " << total << endl;
+
+    cout << " \nTotal cost is " << total << endl;
 }
